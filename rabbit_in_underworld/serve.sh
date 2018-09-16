847a2fe35cde513465459eb5c177ced13f721e91
grok_it <grokit@users.noreply.github.com>
2018-09-15 23:34:13 -0700

    Delete serve.sh

--- rabbit_in_underworld/serve.sh
+++
@@ -1,9 +0,0 @@
-
-# important: --bind 127.0.0.1 make sure it's only accessible from local computer
-# so other people on same network do not have access
-python3 -m http.server 8000 --bind 127.0.0.1
-
-#lsof -i :8000
-#COMMAND   PID USER   FD   TYPE  DEVICE SIZE/OFF NODE NAME
-#python3 28023 arch    4u  IPv4 4147446      0t0  TCP localhost:8000 (LISTEN)
-

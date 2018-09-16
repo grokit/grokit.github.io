c04d614b0696f45155cc580401cda32131cba3e5
grok_it <grokit@users.noreply.github.com>
2018-09-15 23:34:06 -0700

    Delete publish.sh

--- rabbit_in_underworld/publish.sh
+++
@@ -1,6 +0,0 @@
-. cd2d
-rm -rf ~/Downloads/latest
-cp -r ../ ~/Downloads/latest
-rm -rf ~/Downloads/latest/.git
-ssh 35.238.167.8 'rm -rf ~/public/latest'
-scp -r ~/Downloads/latest 35.238.167.8:~/public/latest
